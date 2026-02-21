392414c85006242ee14a75ea71326722963d1e68
Kirill <kirillkopach2006@gmial.com>
2026-02-21 13:31:53 +1000

    .

--- src/shared/api/base.ts
+++ src/shared/api/base.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:3001";
+export const API_URL = "https://test-task-back-8h7wvk31t-kakady2006s-projects.vercel.app/";
 
 export const fetcher = async (url: string) => {
     const response = await fetch(API_URL + url);
